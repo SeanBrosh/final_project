58d455e42b0314902d2367cf408f67ae059d1627
Sean <Seanbr01@gmail.com>
2022-08-11 02:18:02 +0300

    LF_LARP_Rank_Update
    Added rank change options for admin

--- FinalProject-main/src/Pages/AccountAdminManagementContent.jsx
+++ FinalProject-main/src/Pages/AccountAdminManagementContent.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
-
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function AccountAdminManagementContent(props) {
     let{email,name,rank,id} = props
 
+    const [ranks, setRanks] = useState(null);
     const styles = {
         border: '1px solid black', margin:50 , padding:30}
-        const apiUrl = 'http://proj9.ruppin-tech.co.il/api/deleteuser';
+        const apiUrlDelete = 'http://proj9.ruppin-tech.co.il/api/deleteuser';
+        const apiUrlChangeRank = 'http://proj9.ruppin-tech.co.il/api/updateuserrank';
+
+
+        const intputTakerRanks = (event) => {
+            setRanks(event.target.value);
+          }
 
     const btnDeleteUser = () => {
         const userDeleteDetails = {
           User_ID: props.id
         };
-        fetch(apiUrl, {
+        fetch(apiUrlDelete, {
           method: 'DELETE',
           body: JSON.stringify(userDeleteDetails),
           headers: new Headers({
@@ -43,6 +53,45 @@ export default function AccountAdminManagementContent(props) {
       }
     
 
+      const btnChangeRank = () => {
+          if(ranks===null)
+          {
+              alert("You must choose the wanted rank change before pressing change!")
+              return
+          }
+        const userRankUpdateDetails = {
+          User_ID: props.id,
+          User_Rank: ranks
+        };
+        fetch(apiUrlChangeRank, {
+          method: 'PUT',
+          body: JSON.stringify(userRankUpdateDetails),
+          headers: new Headers({
+            'Content-type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json; charset=UTF-8'
+          })
+        }).then(res => {
+          if (res.ok) {
+            return res.json()
+          }
+          else {
+            return null;
+          }
+        }).then((result) => {
+          if (result == null) {
+            alert("Error in Rank Update Process!")
+            return;
+          }
+          alert("User's rank have been succesfully changed!")
+          window.location.reload(false);
+        },
+          (error) => {
+            console.log("err post=", error)
+          });
+
+    
+      }
+
     return (
     <div style={styles}> 
         User Name : {name}<br></br>
@@ -52,6 +101,27 @@ export default function AccountAdminManagementContent(props) {
         User Rank : {rank}
         <br></br>
         <Button style={{margin:30}} color="error" variant="contained" onClick={btnDeleteUser}>Delete</Button><br/>
+        <br></br>
+        <FormControl style={{margin:30}}
+      display="flex"
+  justifyContent="center"
+  alignItems="center"  sx={{ minWidth: 200 }}>
+        <InputLabel id="demo-simple-select-label">Choose the rank.</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={ranks}
+          label="Choose the rank."
+          onChange={intputTakerRanks}
+          
+        >
+          <MenuItem value={"User"}>User</MenuItem>
+          <MenuItem value={"Larp Creator"}>Larp Creator</MenuItem>
+          <MenuItem value={"Moderator"}>Moderator</MenuItem>
+          <MenuItem value={"Banned"}>Banned</MenuItem>
+        </Select>
+      </FormControl> <br></br>
+      <Button style={{margin:30}}  variant="contained" onClick={btnChangeRank}>Change Rank</Button><br/>
         <br></br>
         <br></br>
     </div>
